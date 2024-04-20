93d6d24843a2d55e0df7e4afc946a7d4f4a6277a
Simon Kiragu <simonkiragu04@gmail.com>
2024-04-20 16:16:34 +0300

    Modified file

--- Reading_writing files.py
+++ Reading_writing files.py
@@ -1,7 +1,12 @@
 # We use open functions
 
+ #It takes two primary parameters:
 
+#File path: The file path parameter consists of the filename and directory where the file is located.
 
+#Mode: The mode parameter specifies the purpose of opening the file, such as 'r' for reading, 'w' for writing, or 'a' for appending.
+
+# Using the read method, you can retrieve the complete content of a file
 file1 = open("Example.txt", "r")
 print(file1)
 
@@ -18,7 +23,12 @@ print(file1.name)
 
 
 # To do away with close() all the time we use with
-# We cab use for to iterate through each line
+#advantages of using the 'with' statement are:
+
+#Automatic resource management: The file is guaranteed to be closed when you exit the with block, even if an exception occurs during processing.
+#Cleaner and more concise code: You don't need to explicitly call close(), making your code more readable and less error-prone.
+
+# We can use for to iterate through each line
 
 with open("Example.txt","r") as file2:
     for line in file2:
@@ -29,12 +39,19 @@ with open("Example.txt","r") as file2:
 
 with open("Example.txt","r") as file:
   
-    #Can use method readline() to read the first line or specific indexes
-    content = file.readline()
-    print(content)
-
+    #Can use method readline() to read the contents of yhe file line by line and store it in a list
     content = file.readline(4)
     print(content)
 
-   
+#It can be called multiple times to read subsequent lines.
+    content = file.readline(10)
+    print(content)
+
+#Looping through lines: Typically, you use a while loop to read lines until no more lines are left 
+while True:
+    line = file2.readline()
+    if not line:
+        break
+    print(line)
+    
 
